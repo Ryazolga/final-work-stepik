e81dd575a198448d975473389833a8de7ad61d4b
Alexey Ryazanov <solargatemusic@gmail.com>
2019-07-24 10:18:39 +0500

    page transition options (2 variants)

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,10 +1,14 @@
 from .base_page import BasePage
 from .locators import MainPageLocators
+from .login_page import LoginPage
 
 class MainPage(BasePage):
     def go_to_login(self):
         login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
+        #----alternate page transition
+        #return LoginPage(browser=self.browser, url=self.browser.current_url)
+        #----in test_main_page.py uncomment to line 11 , and comment to line 8,10
 
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Registration link is not presented"
--- test_main_page.py
+++ test_main_page.py
@@ -2,13 +2,14 @@ from pages.main_page import MainPage
 from pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=midsummer"
+    link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)
     page.open()
-    page.should_be_login_link()
     page.go_to_login()
 
-    page_login = LoginPage(browser,  browser.current_url)
-    page_login.should_be_login_url()
-    page_login.should_be_login_form()
-    page_login.should_be_register_form()
+    login_page = LoginPage(browser,  browser.current_url)
+    #login_page = page.go_to_login()
+    login_page.should_be_login_page()
+    login_page.should_be_login_url()
+    login_page.should_be_login_form()
+    login_page.should_be_register_form()
