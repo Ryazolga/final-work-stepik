0e49c5f7280351e8fc871152ec9b56d8bffb8e3e
Alexey Ryazanov <solargatemusic@gmail.com>
2019-07-24 09:14:44 +0500

    Added files locators.py

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,11 @@
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=midsummer" #"http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.go_to_login()
     page.should_be_login_link()
+    #page.should_be_login_form()
+    #page.should_be_register_form()
